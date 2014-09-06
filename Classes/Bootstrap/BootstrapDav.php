401225dd0836f7a43634f721c0ff9377772ac66e
Kay Strobach <info@kay-strobach.de>
2014-09-06 10:06:59 +0200

    [TASK] Add autoloader

--- Classes/Bootstrap/BootstrapDav.php
+++ Classes/Bootstrap/BootstrapDav.php
@@ -16,6 +16,18 @@ class BootstrapDav {
 	 * @return void
 	 */
 	public static function initialize() {
-		require_once (ExtensionManagementUtility::extPath('webdav') . 'Resources/Contrib/SabreDav/lib/Sabre/autoload.php');
+		//require_once (ExtensionManagementUtility::extPath('webdav') . 'Resources/Contrib/SabreDav/lib/Sabre/autoload.php');
+		$autoloader = new BootstrapDav();
+		spl_autoload_register(array($autoloader, 'registerAutoloader'));
+	}
+
+	public function registerAutoloader($className) {
+		if (strpos($className,'Sabre_') === 0) {
+			include (self::getSabeDavBaseDir() . str_replace('_','/',substr($className,6)) . '.php');
+		}
+	}
+
+	public function getSabeDavBaseDir() {
+		return ExtensionManagementUtility::extPath('webdav') . 'Resources/Contrib/SabreDav/lib/Sabre/';
 	}
 } 
